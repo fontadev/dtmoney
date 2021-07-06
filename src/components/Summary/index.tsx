3341b4bcf2179d224a7e0e2419971f9c28e0cb2b
Felipe Fontana <felipefontana0@gmail.com>
2021-07-05 21:59:17 -0300

    TransactionsProvider

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,37 +1,38 @@
-import React,{useContext} from 'react';
-import incomeImg from '../../assets/income.svg'
-import outcomeImg from '../../assets/outcome.svg'
-import totalImg from '../../assets/total.svg'
+import React, { useContext } from "react";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
 import { Container } from "./styles";
-import {TransactionsContext} from '../../TransactionsContext'
+import { TransactionsContext } from "../../TransactionsContext";
 
 export function Summary() {
+  const transactions = useContext(TransactionsContext);
 
-    const data = useContext(TransactionsContext)
+  console.log(transactions);
 
-    return (
-        <Container>
-            <div>
-                <header>
-                    <p>Entradas</p>
-                    <img src={incomeImg} alt="Entradas" />
-                </header>
-                <strong>R$1000,00</strong>
-            </div>
-            <div>
-                <header>
-                    <p>Saídas</p>
-                    <img src={outcomeImg} alt="Saídas" />
-                </header>
-                <strong>- R$500,00</strong>
-            </div>
-            <div className="highlight-background">
-                <header>
-                    <p>Total</p>
-                    <img src={totalImg} alt="Total" />
-                </header>
-                <strong>- R$500,00</strong>
-            </div>
-        </Container>
-    )
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+    </Container>
+  );
 }
